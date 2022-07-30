d2981cf2f43c85c32acb521b80712c507bcd328f
dhrubasaha08 <dhrubasaha@outlook.com>
2022-07-30 10:42:27 +0530

    Update commit message

--- backup.bat
+++ backup.bat
@@ -1,3 +1,21 @@
+:: Script makle a backup of the world folder using 7zip
+:: Copyright (C) 2022  Dhruba Saha
+
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU Affero General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU Affero General Public License for more details.
+
+:: You should have received a copy of the GNU Affero General Public License
+:: along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 for /f "delims=" %%a in ('wmic OS Get localdatetime  ^| find "."') do set dt=%%a
 set YYYY=%dt:~0,4%
 set MM=%dt:~4,2%
